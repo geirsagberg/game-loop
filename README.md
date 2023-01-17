3669b4bfefe12c4b70f7837d5d0276da5184fcac
geirsagberg <geir.sagberg@gmail.com>
2023-01-17 16:15:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# game-loop
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-jvbs2x)
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import './style.css';
+
+const canvas = document.querySelector<HTMLCanvasElement>('canvas')!!;
+
+const context = canvas.getContext('2d');
