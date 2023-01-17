3b261c6b55ea03aacc221a0aa4d3d0938d23586e
Geir Sagberg <geir.sagberg@gmail.com>
2023-01-17 16:27:23 +0100

    Blank canvas

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import './style.css';
+import './style.css'
 
-const canvas = document.querySelector<HTMLCanvasElement>('canvas')!!;
+const canvas = document.querySelector<HTMLCanvasElement>('canvas')!!
 
-const context = canvas.getContext('2d');
+const context = canvas.getContext('2d')!!
+
+context.fillRect(0, 0, canvas.width, canvas.height)
