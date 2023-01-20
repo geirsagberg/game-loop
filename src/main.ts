a9c32c09b647ff96c04859ed0c6a1699d5044131
Geir Sagberg <geir.sagberg@gmail.com>
2023-01-20 17:22:00 +0100

    Game complete

--- src/main.ts
+++ src/main.ts
@@ -12,25 +12,59 @@ type Rect = {
   color: string
 }
 
-const entities: any[] = []
-
-function setup() {
-  const playerSize = 32
-  const player = {
-    rect: {
-      x: (canvas.width - playerSize) / 2,
-      y: (canvas.height - playerSize) / 2,
-      width: playerSize,
-      height: playerSize,
-      color: 'red',
-    },
-  }
-  entities.push(player)
+type Text = {
+  x: number
+  y: number
+  text: string
+  color: string
 }
 
-function draw(rect: Rect) {
+const entities: any[] = []
+const playerSize = 32
+const player = {
+  rect: {
+    x: (canvas.width - playerSize) / 2,
+    y: (canvas.height - playerSize) / 2,
+    width: playerSize,
+    height: playerSize,
+    color: 'red',
+  },
+}
+
+const gameState = {
+  isRunning: true,
+}
+
+const scoreBoard = {
+  text: {
+    x: 10,
+    y: 32,
+    text: 'Score: 0',
+    color: 'white',
+  },
+  score: 0,
+}
+
+function setup() {
+  entities.length = 0
+  player.rect.x = (canvas.width - playerSize) / 2
+  player.rect.y = (canvas.height - playerSize) / 2
+  scoreBoard.score = 0
+  entities.push(player)
+  entities.push(scoreBoard)
+  keysPressed.clear()
+  gameState.isRunning = true
+}
+
+function drawRect(rect: Rect) {
   context.fillStyle = rect.color
   context.fillRect(rect.x, rect.y, rect.width, rect.height)
+}
+
+function drawText(text: Text) {
+  context.fillStyle = text.color
+  context.font = '24px Arial'
+  context.fillText(text.text, text.x, text.y)
 }
 
 function render() {
@@ -38,14 +72,95 @@ function render() {
 
   for (const entity of entities) {
     if (entity.rect) {
-      draw(entity.rect)
+      drawRect(entity.rect)
+    }
+    if (entity.text) {
+      drawText(entity.text)
     }
   }
 }
 
 function update() {
+  if (!gameState.isRunning) {
+    return
+  }
+  spawnCubes()
   movePlayer()
+  moveCubes()
+  incrementScore()
+  checkCollisions()
   wrapEntities()
+}
+
+function checkCollisions() {
+  // Check if the player is colliding with any cubes
+  for (const entity of entities) {
+    if (entity.rect && entity !== player) {
+      if (
+        player.rect.x < entity.rect.x + entity.rect.width &&
+        player.rect.x + player.rect.width > entity.rect.x &&
+        player.rect.y < entity.rect.y + entity.rect.height &&
+        player.rect.y + player.rect.height > entity.rect.y
+      ) {
+        // Collision detected!
+        gameState.isRunning = false
+        alert(`Game over! Your score was ${Math.floor(scoreBoard.score)}`)
+        setup()
+      }
+    }
+  }
+}
+
+function incrementScore() {
+  // Increment score every second that the player is alive
+  scoreBoard.score += 1 / 60
+  scoreBoard.text.text = `Score: ${Math.floor(scoreBoard.score)}`
+}
+
+function moveCubes() {
+  for (const entity of entities) {
+    if (entity.velocity) {
+      entity.rect.x += entity.velocity.x
+      entity.rect.y += entity.velocity.y
+    }
+  }
+}
+
+function spawnCubes() {
+  if (Math.random() < 0.01) {
+    const cubeSize = 32
+    const cube = {
+      velocity: {
+        x: Math.random() * 2 - 1,
+        y: Math.random() * 2 - 1,
+      },
+      rect: {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        width: cubeSize,
+        height: cubeSize,
+        color: getRandomColor(),
+      },
+    }
+    // Make sure the cube doesn't spawn too close to the player
+    if (
+      Math.abs(cube.rect.x - entities[0].rect.x) < 100 &&
+      Math.abs(cube.rect.y - entities[0].rect.y) < 100
+    ) {
+      return
+    }
+
+    entities.push(cube)
+  }
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
 }
 
 function wrapEntities() {
