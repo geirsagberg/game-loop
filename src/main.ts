76cad0df549018b367685b06d8b0d84a8ba907de
Geir Sagberg <geir.sagberg@gmail.com>
2023-01-20 17:02:35 +0100

    Movable player

--- src/main.ts
+++ src/main.ts
@@ -12,37 +12,86 @@ type Rect = {
   color: string
 }
 
-const rects: Rect[] = []
+const entities: any[] = []
 
 function setup() {
-  rects.push({
-    x: 0,
-    y: 0,
-    width: 32,
-    height: 32,
-    color: 'red',
-  })
+  const playerSize = 32
+  const player = {
+    rect: {
+      x: (canvas.width - playerSize) / 2,
+      y: (canvas.height - playerSize) / 2,
+      width: playerSize,
+      height: playerSize,
+      color: 'red',
+    },
+  }
+  entities.push(player)
+}
+
+function draw(rect: Rect) {
+  context.fillStyle = rect.color
+  context.fillRect(rect.x, rect.y, rect.width, rect.height)
 }
 
 function render() {
   context.clearRect(0, 0, canvas.width, canvas.height)
-  for (const rect of rects) {
-    context.fillStyle = rect.color
-    context.fillRect(rect.x, rect.y, rect.width, rect.height)
+
+  for (const entity of entities) {
+    if (entity.rect) {
+      draw(entity.rect)
+    }
   }
 }
 
 function update() {
-  for (const rect of rects) {
-    rect.x += 1
-    rect.y += 1
-    if (rect.x > canvas.width) {
-      rect.x = 0
-    }
-    if (rect.y > canvas.height) {
-      rect.y = 0
+  movePlayer()
+  wrapEntities()
+}
+
+function wrapEntities() {
+  for (const entity of entities) {
+    if (entity.rect) {
+      if (entity.rect.x > canvas.width) {
+        entity.rect.x = 0
+      }
+      if (entity.rect.x < 0) {
+        entity.rect.x = canvas.width
+      }
+      if (entity.rect.y > canvas.height) {
+        entity.rect.y = 0
+      }
+      if (entity.rect.y < 0) {
+        entity.rect.y = canvas.height
+      }
     }
   }
+}
+
+const keysPressed = new Set<string>()
+
+function movePlayer() {
+  if (keysPressed.has('ArrowLeft') || keysPressed.has('a')) {
+    entities[0].rect.x -= 5
+  }
+  if (keysPressed.has('ArrowRight') || keysPressed.has('d')) {
+    entities[0].rect.x += 5
+  }
+  if (keysPressed.has('ArrowUp') || keysPressed.has('w')) {
+    entities[0].rect.y -= 5
+  }
+  if (keysPressed.has('ArrowDown') || keysPressed.has('s')) {
+    entities[0].rect.y += 5
+  }
+}
+
+function handleInput() {
+  // Read keys and add them to the set
+  document.addEventListener('keydown', (event) => {
+    keysPressed.add(event.key)
+  })
+  document.addEventListener('keyup', (event) => {
+    keysPressed.delete(event.key)
+  })
 }
 
 setup()
@@ -62,6 +111,7 @@ setup()
 // }
 
 function loop() {
+  handleInput()
   update()
   render()
   requestAnimationFrame(loop)
